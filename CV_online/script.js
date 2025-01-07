39e32a9045f5c80f6148681296dcd1d29ebd7a16
Lysell92 <157490830+Lysell92@users.noreply.github.com>
2025-01-07 12:07:34 +0100

    Add project files.

---
+++ CV_online/script.js
@@ -0,0 +1,10 @@
+﻿function validateCaptcha() {
+    var captchaInput = document.getElementById('captcha').value;
+    var correctAnswer = 5; // The correct answer to 2 + 3
+
+    if (parseInt(captchaInput) === correctAnswer) {
+        alert("Formularen er valideret. (Formularen sender ikke data).");
+    } else {
+        alert("Forkert svar på CAPTCHA. Prøv igen.");
+    }
+}
